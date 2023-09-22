028eed636a5d28abe37d5a3d67f68808755d2e5b
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-09-22 21:50:50 +0300

    removed unused plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2023 HarryPotterAndroid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
-    alias(libs.plugins.gms)
     alias(libs.plugins.mapsplatform.secrets.gradle.plugin)
     alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
     alias(libs.plugins.io.gitlab.arturbosch.detekt)
@@ -98,5 +112,4 @@ dependencies {
     testImplementation(libs.test.robolectric)
     testImplementation(libs.compose.ui.test.junit)
     androidTestImplementation(libs.compose.ui.test.junit)
-
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.gms) apply false
     alias(libs.plugins.mapsplatform.secrets.gradle.plugin) apply false
     alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization) apply false
     alias(libs.plugins.org.jlleitschuh.gradle.ktlint)
