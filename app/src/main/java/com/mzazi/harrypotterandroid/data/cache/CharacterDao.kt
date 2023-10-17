ddbda1b89a578cf05b1af3567361c094428790a9
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-10-17 14:59:18 +0300

    setup dao

---
+++ app/src/main/java/com/mzazi/harrypotterandroid/data/cache/CharacterDao.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023 HarryPotterAndroid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mzazi.harrypotterandroid.data.cache
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface CharacterDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCharacters(list: List<CharacterEntity>): List<Long>
+
+    @Query("SELECT * from characters")
+    fun getCharacter(): List<CharacterEntity>
+
+    @Query("SELECT * from characters where id Like :id")
+    fun getCharacterById(id: String): CharacterEntity
+}
