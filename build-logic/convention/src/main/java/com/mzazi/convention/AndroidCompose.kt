bbde033fa12495b1668ba9c7869ce8b7b3f5ca25
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-11-30 22:38:37 +0300

    Adds dependency to AndroidCompose.kt

--- build-logic/convention/src/main/java/com/mzazi/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/java/com/mzazi/convention/AndroidCompose.kt
@@ -4,6 +4,7 @@ import com.android.build.api.dsl.CommonExtension
 import java.io.File
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
 /**
@@ -21,6 +22,11 @@ internal fun Project.configureAndroidCompose(
 
         composeOptions {
             kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
+        }
+        dependencies {
+            val bom = libs.findLibrary("androidx-compose-bom").get()
+            add("implementation", platform(bom))
+            add("androidTestImplementation", platform(bom))
         }
 
         kotlinOptions {
